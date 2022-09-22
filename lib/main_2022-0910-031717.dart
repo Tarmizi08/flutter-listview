bd76939751f9d723ecb65fe580f79462e67fd98d
tarmizi <mohammadtarmiziismail@gmail.com>
2022-09-22 22:55:17 +0800

    initial version

---
+++ lib/main_2022-0910-031717.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(),
+      home: RootPage(),
+    );
+  }
+}
+
+// this to reload the page
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
